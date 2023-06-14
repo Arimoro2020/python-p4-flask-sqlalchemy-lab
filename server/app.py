940692d60ec7af3ee69dffac9ca55b5b413ad5a4
Adeyemi Arimoro <arimoro71@gmail.com>
2023-06-13 22:03:45 -0500

    Add solution

--- server/app.py
+++ server/app.py
@@ -19,15 +19,106 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    q = Animal.query.filter(Animal.id == id).first()
+
+    if not q:
+        response_body = '<h1>404 animal not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+    else:
+        response_body = f'<ul>Name: {q.name}</ul>\n'
+        response_body += f'<ul>Species: {q.species}</ul>\n'
+
+        
+    q_zookeepers = Zookeeper.query.filter(Zookeeper.id ==q.zookeeper_id).all()
+
+
+
+    if not q_zookeepers:
+        response_body += '<h1>404 No zookeeper found</h1>'
+        response = make_response(response_body, 404)
+
+    else:
+        for qz in range(len(q_zookeepers)):
+             response_body += f'<ul>Zookeeper: {q_zookeepers[qz].name}</ul>'
+
+    
+          
+    q_enclosures = Enclosure.query.filter(Enclosure.id == q.enclosure_id).all()
+
+
+
+    if not q_enclosures:
+        response_body += '<h1>404 No enclosure found</h1>'
+        response = make_response(response_body, 404)
+
+    else:
+        for qe in range(len(q_enclosures)):
+             response_body += f'<ul>Enclosure: {q_enclosures[qe].environment}</ul>'
+        
+
+    return make_response(response_body, 200)
+
+    
+    
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    q = Zookeeper.query.filter(Zookeeper.id == id).first()
+    if not q:
+        response_body = '<h1>404 zookeeper not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+    
+    Name = q.name
+    Birthday = q.birthday
+
+    response_body = f'<ul>Name: {Name}</ul>\n'
+    response_body += f'<ul>Birthday: {Birthday}</ul>\n'
+    
+    q_animals = Animal.query.filter(Animal.zookeeper_id ==id).all()
+
+
+
+    if not q_animals:
+        response_body += '<h1>404 No animal found</h1>'
+        response = make_response(response_body, 404)
+
+    else:
+        for q in range(len(q_animals)):
+             response_body += f'<ul>Animal: {q_animals[q].name}</ul>'
+        
+
+    return make_response(response_body, 200)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+
+    q = Enclosure.query.filter(Enclosure.id == id).first()
+
+    if not q:
+        response_body = '<h1>404 enclosure not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+   
+   
+    response_body = f'<ul>Environment: {q.environment}</ul>\n'
+    response_body += f'<ul>Open to visitors: {q.open_to_visitors}</ul>\n'
+
+    q_animals = Animal.query.filter(Animal.enclosure_id == id).all()
+
+
+
+    if not q_animals:
+        response_body += '<h1>404 No animal found</h1>'
+        response = make_response(response_body, 404)
+
+    else:
+        for qa in range(len(q_animals)):
+             response_body += f'<ul>Animal: {q_animals[qa].name}</ul>'
+
+
+    return make_response(response_body, 200)
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from sqlalchemy.ext.associationproxy import association_proxy
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -11,13 +12,29 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.DateTime)
+    start_date = db.Column(db.DateTime, server_default= db.func.now())
+    end_date = db.Column(db.DateTime, onupdate=db.func.now())
+    zookeeper_animals= db.relationship('Animal', back_populates='zookeeper')
+    enclosures= association_proxy('zookeeper_animals', 'enclosure')
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean, default=False)
+    enclosure_animals = db.relationship('Animal', back_populates='enclosure')
+    zookeepers = association_proxy('enclosure_animals', 'zookeeper')
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)  
+    species = db.Column(db.String)
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+    enclosure = db.relationship('Enclosure', back_populates='enclosure_animals')
+    zookeeper = db.relationship('Zookeeper', back_populates='zookeeper_animals')
